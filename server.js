6fa4fc2a8d24bcd341ce55d94d403b9645836405
saiprashanth88 <saiprashanth817@gmail.com>
2023-02-13 22:19:36 +0530

    Serving Html file

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
-	res.send("Iam Sai Prashanth");
+	// res.send("Iam Sai Prashanth");
+	res.sendFile(__dirname+"/index.html")
 });
 
 app.listen(port, function(){
